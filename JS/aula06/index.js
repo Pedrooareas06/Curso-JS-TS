2fcf1e6acda69b1540184200917d518e31c60090
Pedro <pedrooareas06@gmail.com>
2026-05-01 17:25:10 -0300

    Tipos de dados primitivos , aula 06

---
+++ JS/aula06/index.js
@@ -0,0 +1,16 @@
+// Tipos de dados primitivos -> string, number , undefined, null, boolean, symbol, bigint
+
+const nome  = 'Pedro Henrique';  // isso é uma string , string são dados representado por  texto , dentro de aspas simples , duplas ou crase . 
+
+const idade = 20; // Isso é um number, number são dados representados por números inteiros ou decimais . 
+
+const estachovendo = true; // Isso é um booelan, boolean são dados representados por true ou false, ou seja pode está chovendo ou não , pode ser sim ou não .
+
+let nomeAluno; // Isso é um undefned, undefine são variaves que foram declaras mais não possuem um valor definido . 
+
+const sobrenomeAluno = null;  // Isso é Nulo , null são valores varioz ou valor nulo , ou seja não aponta para  local nenhum na memoria . 
+
+const simbolo = Symbol('Pedro Henrique'); // Isso é um symbol, symbol são dados representados por um valor único e imutável , ou seja não pode ser alterado .
+
+const bigInt = 123456789012345678901234567890n; // Isso é um bigint, bigint são dados representados por números inteiros muito grandes , ou seja não tem limite de tamanho .
+
