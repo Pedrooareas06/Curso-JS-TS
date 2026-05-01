931ae683a5157ec9660fa6c61e492757b6ad73bb
Pedro <pedrooareas06@gmail.com>
2026-05-01 16:56:02 -0300

    ex aula 05

---
+++ JS/aula05/exercicio.js
@@ -0,0 +1,9 @@
+const nome = 'Pedro Henrique';
+const sobreNome = "De Lima Areas";
+const idade = 20;
+const peso = 68;
+const altura = 1.72;
+let imc = peso / (altura * altura);
+let anoNascimento =  2026 - idade;
+
+console.log(nome, sobreNome, 'tem', idade, 'anos', 'pesa', peso, 'kg e tem', altura, 'cm de altura e seu IMC é', imc, 'e nasceu em', anoNascimento);
